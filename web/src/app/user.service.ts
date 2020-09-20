20342c8d9a7d9566f3fd755e1019097222c58180
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-20 11:13:43 +0530

    Added heroku

--- web/src/app/user.service.ts
+++ web/src/app/user.service.ts
@@ -8,7 +8,8 @@ import { Observable } from 'rxjs';
 export class UserService {
   constructor(private http: HttpClient) {}
 
-  url = 'http://127.0.0.1:8000/api';
+  // url = 'http://127.0.0.1:8000/api';
+  url = 'https://bharath-django-angular.herokuapp.com/api';
 
   registerUser(userData): Observable<any> {
     return this.http.post(`${this.url}/users/`, userData);
